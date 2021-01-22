ec975b2a67d7db24a9e4e8968bbbad21ea2210ea
k.kulkov <k.kulkov@initlab.ru>
2021-01-22 08:52:23 +0300

    fix upload_to_yandex_s3.sh

--- bash/upload_to_yandex_s3.sh
+++ bash/upload_to_yandex_s3.sh
@@ -1,11 +1,11 @@
 #/bin/sh
-# $2 - path to backup
+BACKUP_LOCAL_PATH="/path/to/local/backup"
 BUCKET_NAME="backet_name"
 BACKUP_NAME="backup_site_name"
 DATE=$(date +%Y%m%d)
 
-/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 cp $2/*$DATE.tar.gz s3://$BUCKET_NAME/$BACKUP_NAME/
-/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 cp $2/*$DATE.sql.gz s3://$BUCKET_NAME/$BACKUP_NAME/
+/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 cp $BACKUP_LOCAL_PATH/*$DATE.tar.gz s3://$BUCKET_NAME/$BACKUP_NAME/
+/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 cp $BACKUP_LOCAL_PATH/*$DATE.sql.gz s3://$BUCKET_NAME/$BACKUP_NAME/
 /usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 ls s3://$BUCKET_NAME/$BACKUP_NAME/ | while read -r line;
   do
     createDate=`echo $line|awk {'print $1" "$2'}`
