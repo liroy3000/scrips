21f9b5c32c78a491665416fe467ea08d0ae96b4e
k.kulkov <k.kulkov@initlab.ru>
2021-01-22 08:49:41 +0300

    fix upload_to_yandex_s3.sh

--- bash/upload_to_yandex_s3.sh
+++ bash/upload_to_yandex_s3.sh
@@ -1,11 +1,12 @@
 #/bin/sh
 # $2 - path to backup
-bucket_name="backet_name"
-backup_name="backup_site_name"
+BUCKET_NAME="backet_name"
+BACKUP_NAME="backup_site_name"
+DATE=$(date +%Y%m%d)
 
-/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 cp $2/*$DATE.tar.gz s3://$bucket_name/$backup_name/
-/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 cp $2/*$DATE.sql.gz s3://$bucket_name/$backup_name/
-/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 ls s3://$bucket_name/$backup_name/ | while read -r line;
+/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 cp $2/*$DATE.tar.gz s3://$BUCKET_NAME/$BACKUP_NAME/
+/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 cp $2/*$DATE.sql.gz s3://$BUCKET_NAME/$BACKUP_NAME/
+/usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 ls s3://$BUCKET_NAME/$BACKUP_NAME/ | while read -r line;
   do
     createDate=`echo $line|awk {'print $1" "$2'}`
     createDate=`date -d"$createDate" +%s`
@@ -17,7 +18,7 @@ backup_name="backup_site_name"
       fileName=`echo $line|awk {'print $4'}`
       if [ $fileName != "" ]
         then
-        /usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 rm s3://$bucket_name/$backup_name/$fileName
+        /usr/local/bin/aws --endpoint-url=https://storage.yandexcloud.net s3 rm s3://$BUCKET_NAME/$BACKUP_NAME/$fileName
         fi
       fi
   done;
